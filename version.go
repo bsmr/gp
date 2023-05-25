6195b36915a1b6847033a9e3e5f2ee31c13e99e1
Boris Mühmer <boris.muehmer@gmail.com>
2023-05-25 21:36:45 +0200

    changed version handline

--- version.go
+++ version.go
@@ -1,9 +1,12 @@
 package gp
 
-const (
-	version = "0.0.2"
+var (
+	// set version during build
+	// -ldflags "-X github.com/bsmr/gp.VersionText=v1.2.3"
+	// go build -ldflags "-X github.com/bsmr/gp.VersionText=$(git branch --show-current)" -o bin/gp cmd/gp/main.go
+	VersionText = "*unspecified version*"
 )
 
 func Version() string {
-	return version
+	return VersionText
 }
